992cf0dba8c6fdbe3c69020e21cbbfb6b7cbfd59
Manuel Gallardo <121906123+manu-mx@users.noreply.github.com>
2023-04-10 17:00:59 -0600

    9mg9gjf9p

--- js/saludo.js
+++ js/saludo.js
@@ -10,3 +10,60 @@ salutdo.textContent = 'Buenas tardes';
 if(hora >= 18 && hora < 24){
 salutdo.textContent = 'Buenas noches';
 };
+//Código para Consultor de aires acondicionados//
+const airesAcondicionados = [
+    {
+      marca: "Samsung",
+      modelo: "AR18NVFSCWK",
+      capacidad: "18.000 BTU",
+      precio: 1000,
+      descripcion: "Aire acondicionado Samsung con capacidad de 18.000 BTU. Viene con tecnología de enfriamiento rápido y un filtro de alta densidad para atrapar polvo y bacterias."
+    },
+    {
+      marca: "LG",
+      modelo: "VM242C6",
+      capacidad: "24.000 BTU",
+      precio: 1200,
+      descripcion: "Aire acondicionado LG con capacidad de 24.000 BTU. Viene con tecnología de enfriamiento rápido y un modo de ahorro de energía para reducir tus costos de electricidad."
+    },
+    {
+      marca: "Mitsubishi",
+      modelo: "MSZ-GE24NA",
+      capacidad: "24.000 BTU",
+      precio: 1300,
+      descripcion: "Aire acondicionado Mitsubishi con capacidad de 24.000 BTU. Viene con tecnología de enfriamiento rápido y un filtro anti-alergias para reducir la cantidad de alérgenos en el aire."
+    }
+  ];
+  
+  let indiceActual = 0;
+  
+  function mostrarAireAcondicionado() {
+    const info = document.getElementById("info");
+    info.innerHTML = "";
+    const aireAcondicionado = airesAcondicionados[indiceActual];
+    const div = document.createElement("div");
+    div.innerHTML = `<h3>${aireAcondicionado.marca} ${aireAcondicionado.modelo}</h3><p><strong>Capacidad:</strong> ${aireAcondicionado.capacidad}</p><p><strong>Precio:</strong> $${aireAcondicionado.precio.toFixed(2)}</p><p><strong>Descripción:</strong> ${aireAcondicionado.descripcion}</p>`;
+    info.appendChild(div);
+  }
+  
+  function mostrarSiguiente() {
+    if (indiceActual < airesAcondicionados.length - 1) {
+      indiceActual++;
+      mostrarAireAcondicionado();
+    }
+  }
+  
+  function mostrarAnterior() {
+    if (indiceActual > 0) {
+      indiceActual--;
+      mostrarAireAcondicionado();
+    }
+  }
+  
+  const siguiente = document.getElementById("siguiente");
+  siguiente.addEventListener("click", mostrarSiguiente);
+  
+  const anterior = document.getElementById("anterior");
+  anterior.addEventListener("click", mostrarAnterior);
+  
+  mostrarAireAcondicionado();  
