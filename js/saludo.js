db899e5988eefc2cc50cbbcd124093d464afdb11
Manuel Gallardo <121906123+manu-mx@users.noreply.github.com>
2023-04-10 15:54:02 -0600

    um8789p

--- js/saludo.js
+++ js/saludo.js
@@ -15,7 +15,7 @@ const toastTrigger = document.getElementById('liveToastBtn')
 const toastLiveExample = document.getElementById('liveToast')
 
 if (toastTrigger) {
-  const toastBootstrap = toastBootstrap.Toast.getOrCreateInstance(toastLiveExample)
+  const toastBootstrap = document.Toast.getOrCreateInstance(toastLiveExample)
   toastTrigger.addEventListener('click', () => {
     toastBootstrap.show()
   })
